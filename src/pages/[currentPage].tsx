99ae943bb55108a8c9977a582c54762f4c6e5252
Accel <58154200+Accel-learn-how-to-code@users.noreply.github.com>
2021-04-21 00:16:19 +0700

    add advance  pagination

--- src/pages/[currentPage].tsx
+++ src/pages/[currentPage].tsx
@@ -1,17 +1,26 @@
+import { openDB } from "../openDB";
 import Home, { getStaticProps } from "./index";
 
 export default Home;
 export { getStaticProps };
 
 export const getStaticPaths = async () => {
+  const db = await openDB();
+  const { total }: any = await db.get(
+    "select count(*) as total from microphone"
+  );
+  const numberOfPages = Math.ceil(total / 5.0)
+  //console.log(numberOfPages)
+  const paths = Array(numberOfPages - 1).fill(' ').map((x, index) => {
+    return {
+      params: {
+        currentPage: (index + 1).toString(),
+      },
+    };
+  });
+  //console.log(paths);
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          currentPage: "2",
-        },
-      },
-    ],
+    paths,
   };
 };
